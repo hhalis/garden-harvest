a900cfb70f3c97d689fec4e8a2fc2cd35846a408
hhalis <ilahameed@hotmail.com>
2025-07-14 12:36:27 +0500

    Add files via upload

---
+++ android/app/src/main/java/com/gardenalchemy/mergemysteries/MainActivity.java
@@ -0,0 +1,7 @@
+package com.gardenalchemy.mergemysteries;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    // No additional code needed - Capacitor handles everything
+}
